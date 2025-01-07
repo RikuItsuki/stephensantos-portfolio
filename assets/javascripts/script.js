48359b37809924cd5d070b3d97eaf47466548359
Stephen Santos <stephenriku.04@gmail.com>
2025-01-07 23:00:40 +0800

    Add Responsive Bootstrap 4

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -44,9 +44,24 @@ function toggleSection(element) {
     
     sections.forEach((section) => {
         if (section === element.id) {
-            document.querySelector(`.${section}`).style.display = "block";
+            document.querySelector(`.${section}`).classList.remove("d-none")
         } else {
-            document.querySelector(`.${section}`).style.display = "none";
+            document.querySelector(`.${section}`).classList.add("d-none")
         }
     });
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
 }
